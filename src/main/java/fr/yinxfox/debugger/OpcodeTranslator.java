6f748f2409e8768fcf1abc9edd277bb35e696491
joshuadwightred <tollemerkevin@gmail.com>
2024-05-23 16:48:25 +0200

    Updated OpcodeTranslator for SChip instructions

--- src/main/java/fr/yinxfox/debugger/OpcodeTranslator.java
+++ src/main/java/fr/yinxfox/debugger/OpcodeTranslator.java
@@ -1,29 +1,54 @@
 package fr.yinxfox.debugger;
 
-public class OpcodeTranslator {
+import fr.yinxfox.Launcher;
+import fr.yinxfox.emulator.Hardware;
 
-    //TODO: update to match SCHIP opcodes
+public class OpcodeTranslator {
 
     public static String decodeOp(int opcode) {
         switch (opcode >> 12) {
             case 0x0 -> {
-                if (opcode == 0x00E0 || opcode == 0x0230) return "CLEAR";
-                else return "RETURN";
+                switch (opcode) {
+                    case 0x00E0, 0x0230 -> {
+                        return "CLEAR";
+                    }
+                    case 0x00EE -> {
+                        return "RETURN";
+                    }
+                    case 0x00FD -> {
+                        return "EXIT";
+                    }
+                    case 0x00FE -> {
+                        return "HIGH RES ON";
+                    }
+                    case 0x00FF -> {
+                        return "HIGH RES OFF";
+                    }
+                    case 0x00FB -> {
+                        return "SCROLL RIGHT";
+                    }
+                    case 0x00FC -> {
+                        return "SCROLL LEFT";
+                    }
+                    default -> {
+                        if ((opcode & 0xFFF0) == 0x00C0) return "SCROLL DOWN " + (opcode & 0x000F);
+                    }
+                }
             }
             case 0x1 -> {
-                return "JMP " + String.format("%03X", opcode & 0x0FFF);
+                return "JUMP " + String.format("%03X", opcode & 0x0FFF);
             }
             case 0x2 -> {
-                return "CAL " + String.format("%03X", opcode & 0x0FFF);
+                return "FUNC " + String.format("%03X", opcode & 0x0FFF);
             }
             case 0x3 -> {
-                return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "==" + String.format("%02X", opcode & 0x00FF);
+                return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "==" + String.format("%02X", opcode & 0x00FF);
             }
             case 0x4 -> {
-                return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "!=" + String.format("%02X", opcode & 0x00FF);
+                return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "!=" + String.format("%02X", opcode & 0x00FF);
             }
             case 0x5 -> {
-                return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "==v" + String.format("%01X", (opcode & 0x00F0) >> 4);
+                return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "==v" + String.format("%01X", (opcode & 0x00F0) >> 4);
             }
             case 0x6 -> {
                 return "SET v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " " + String.format("%02X", opcode & 0x00FF);
@@ -63,25 +88,34 @@ public class OpcodeTranslator {
                 }
             }
             case 0x9 -> {
-                return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "!=v" + String.format("%01X", (opcode & 0x00F0) >> 4);
+                return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "!=v" + String.format("%01X", (opcode & 0x00F0) >> 4);
             }
             case 0xA -> {
                 return "SET I " + String.format("%03X", opcode & 0x0FFF);
             }
             case 0xB -> {
-                return "JMP " + String.format("%03X", opcode & 0x0FFF) + " v0";
+                if (Launcher.getHardware() == Hardware.CHIP8 || Launcher.getHardware() == Hardware.CHIP8HIRES) {
+                    return "JUMP " + String.format("%03X", opcode & 0x0FFF) + " v0";
+                } else {
+                    return "JUMP " + String.format("%02X", opcode & 0x00FF) + " v" + String.format("%01X", (opcode & 0x0F00) >> 8);
+                }
             }
+
             case 0xC -> {
                 return "SET v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " rand " + String.format("%02X", opcode & 0x00FF);
             }
             case 0xD -> {
-                return "DRW v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " v" + String.format("%01X", (opcode & 0x00F0) >> 4) + " " + String.format("%01X", opcode & 0x000F);
+                if ((opcode & 0x000F) != 0) {
+                    return "DRAW v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " v" + String.format("%01X", (opcode & 0x00F0) >> 4) + " " + String.format("%01X", opcode & 0x000F);
+                } else {
+                    return "DRAW v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " v" + String.format("%01X", (opcode & 0x00F0) >> 4) + " 16";
+                }
             }
             case 0xE -> {
                 if ((opcode & 0x00FF) == 0x9E) {
-                    return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "PRSD";
-                } else {
-                    return "SKP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + "NPRSD";
+                    return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " PRSD";
+                } else if ((opcode & 0x00FF) == 0xA1) {
+                    return "SKIP v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " NPRSD";
                 }
             }
             case 0xF -> {
@@ -102,7 +136,10 @@ public class OpcodeTranslator {
                         return "ADD I v" + String.format("%01X", (opcode & 0x0F00) >> 8);
                     }
                     case 0x29 -> {
-                        return "SET I v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " HEX";
+                        return "SET I v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " FONT";
+                    }
+                    case 0x30 -> {
+                        return "SET I v" + String.format("%01X", (opcode & 0x0F00) >> 8) + " FONT H";
                     }
                     case 0x33 -> {
                         return "BCD v" + String.format("%01X", (opcode & 0x0F00) >> 8);
@@ -112,6 +149,12 @@ public class OpcodeTranslator {
                     }
                     case 0x65 -> {
                         return "LOAD v" + String.format("%01X", (opcode & 0x0F00) >> 8);
+                    }
+                    case 0x75 -> {
+                        return "SAVE rpl v" + String.format("%01X", (opcode & 0x0F00) >> 8);
+                    }
+                    case 0x85 -> {
+                        return "LOAD rpl v" + String.format("%01X", (opcode & 0x0F00) >> 8);
                     }
                 }
             }
